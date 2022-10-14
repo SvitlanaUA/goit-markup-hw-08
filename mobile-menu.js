88d4311cb1713e7fce68730b36bd1a9072a7e69e
SvitlanaUA <111605488+SvitlanaUA@users.noreply.github.com>
2022-10-14 22:38:42 +0300

    ДЗ_8

---
+++ mobile-menu.js
@@ -0,0 +1,13 @@
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
+
+  // Закрываем мобильное меню на более широких экранах
+  // в случае изменения ориентации устройства.
+  // Close the mobile menu on wider screens if the device orientation changes
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
+})();
